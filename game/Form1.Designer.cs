6e17d437473b8d4cf0eb21f8de2c36a8816b771e
Braxton Daeschle <braxdaes073@ed.amdsb.ca>
2023-01-25 10:40:20 -0500

    Add project files.

---
+++ game/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.winLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.subtitleLabel = new System.Windows.Forms.Label();
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.Location = new System.Drawing.Point(211, 13);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(0, 13);
+            this.winLabel.TabIndex = 0;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(497, 122);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(0, 13);
+            this.titleLabel.TabIndex = 1;
+            // 
+            // subtitleLabel
+            // 
+            this.subtitleLabel.AutoSize = true;
+            this.subtitleLabel.Location = new System.Drawing.Point(175, 107);
+            this.subtitleLabel.Name = "subtitleLabel";
+            this.subtitleLabel.Size = new System.Drawing.Size(0, 13);
+            this.subtitleLabel.TabIndex = 2;
+            // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.AutoSize = true;
+            this.p1ScoreLabel.Location = new System.Drawing.Point(425, 12);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(0, 13);
+            this.p1ScoreLabel.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.p1ScoreLabel);
+            this.Controls.Add(this.subtitleLabel);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.winLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label subtitleLabel;
+        private System.Windows.Forms.Label p1ScoreLabel;
+    }
+}
+
---
+++ game/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        Random rangen = new Random();
+        Rectangle player1 = new Rectangle(295, 195, 10, 10);
+        
+        Rectangle Point = new Rectangle(295, 150, 10, 10);
+        Rectangle Speed = new Rectangle(295, 50, 10, 10);
+
+        int player1Score = 0;
+    
+
+        int playerSpeed = 4;
+       
+        int ballXSpeed = -6;
+        int ballYSpeed = 6;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool aDown = false;
+        bool dDown = false;
+        bool rightArrowDown = false;
+        bool leftArrowDown = false;
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush greenBrush = new SolidBrush(Color.Green);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
+
+        string gameState = "waiting";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void GameSetup()
+        {
+            gameState = "running";
+
+             titleLabel.Text = "";
+            subtitleLabel.Text = "";
+
+            timer1.Enabled = true;
+            //time = 500;
+            //score = 0;
+
+            //hero.X = 280;
+
+            //balls.Clear();
+            //ballSpeeds.Clear();
+            //ballColours.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Space:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        GameSetup();
+                    }
+                    break;
+                case Keys.Escape:
+                    if (gameState == "waiting" || gameState == "over")
+                    {
+                        this.Close();
+                    }
+                    break;
+
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (gameState == "waiting")
+            {
+
+
+
+                titleLabel.Text = "Catch Game";
+                subtitleLabel.Text = "Press Space to Start or Esc to Exit";
+            }
+            else if (gameState == "running")
+            {
+                e.Graphics.FillRectangle(blueBrush, player1);
+               
+                e.Graphics.FillRectangle(whiteBrush, Point);
+                e.Graphics.FillRectangle(redBrush, Speed);
+            }
+            else if (gameState == "over")
+            {
+
+
+                titleLabel.Text = "Game Over";
+                subtitleLabel.Text = "Press Space to Start or Esc to Exit";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //move player 1 
+            if (wDown == true && player1.Y > 0)
+            {
+                player1.Y -= playerSpeed;
+            }
+
+            if (sDown == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += playerSpeed;
+            }
+            if (dDown == true && player1.X < this.Width - player1.Width)
+            {
+                player1.X += playerSpeed;
+            }
+            if (aDown == true && player1.X > 0)
+            {
+                player1.X -= playerSpeed;
+            }
+
+
+           
+
+            //check if ball hit top or bottom wall and change direction if it does 
+            if (Point.Y < 0 || Point.Y > this.Height - Point.Height)
+            {
+                ballYSpeed *= -1;  // or: ballYSpeed = -ballYSpeed; 
+            }
+
+            //check if ball hits either player. If it does change the direction 
+            //and place the ball in front of the player hit 
+            if (player1.IntersectsWith(Point))
+            {
+                int POINTX = rangen.Next(0, 662);
+                int POINTY = rangen.Next(0, 449);
+                Point.X = POINTX;
+                Point.Y = POINTY;
+                player1Score++;
+                p1ScoreLabel.Text = $"{player1Score}";
+            }
+           
+            if (player1.IntersectsWith(Speed))
+            {
+                int sppedX = rangen.Next(0, 662);
+                int speedY = rangen.Next(0, 449);
+                Speed.X = sppedX;
+                Speed.Y = speedY;
+                playerSpeed++;
+            }
+
+            //check if a player missed the ball and if true add 1 to score of other player  
+            if (Point.X < 0)
+            {
+                player1Score++;
+                p1ScoreLabel.Text = $"{player1Score}";
+
+                Point.X = 295;
+                Point.Y = 195;
+
+                player1.Y = 170;
+                
+
+            }
+            else if (Point.X > 600)
+            {
+                
+            }
+
+            // check score and stop game if either player is at 3 
+            if (player1Score == 10)
+            {
+                timer1.Enabled = false;
+                winLabel.Visible = true;
+                winLabel.Text = "Player 1 Wins!!";
+            }
+           
+            
+
+
+
+            Refresh();
+        }
+
+        
+        
+    }
+}
